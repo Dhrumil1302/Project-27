545250cf9e5aee55f7c2adc7e2e95bf7abd53c63
Dhrumil1302 <72877157+Dhrumil1302@users.noreply.github.com>
2020-12-29 20:48:59 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,71 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint = Matter.Constraint
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+	
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	roof = new Roof(200,20,1700,30);
+	bob1= new Bob(100,400);
+	bob2 = new Bob(300,400);
+	bob3 = new Bob(500,400);
+	bob4 = new Bob(700,400);
+	bob5 = new Bob(900,400);
+	bob6 = new Bob(1220,400);
+	rope1 = new Rope(bob1.body,roof.body,{x:40,y:20});
+	rope2 = new Rope(bob2.body,roof.body,{x:80,y:20});
+    rope3 = new Rope(bob3.body,roof.body,{x:120,y:20});
+    rope4 = new Rope(bob4.body,roof.body,{x:160,y:20});
+	rope5 = new Rope(bob5.body,roof.body,{x:200,y:20});
+	rope6 = new Rope(bob6.body,roof.body,{x:240,y:20});
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(255);
+  
+  roof.display();
+  bob2.display();
+  bob1.display();
+  bob3.display();
+  bob4.display(); 
+ bob5.display(); 
+ bob6.display(); 
+ rope1.display();
+rope2.display();
+  rope3.display();
+  rope4.display();
+  rope5.display();	
+ rope6.display(); 
+  drawSprites();
+ 
+}
+
+
+function keyPressed() {
+	if (keyCode === UP_ARROW) {
+
+	  Matter.Body.applyForce(bob1.body,bob1.body.position,{x:-50,y:-45});
+
+	}
+}
+
+
+
